a098211d2ef7789948f4d477ce92f57e5b10d085
alexisc64 <78241735+alexisc64@users.noreply.github.com>
2024-04-13 00:40:37 -0600

    Add files via upload
    excercise 5 done

---
+++ Part5/Setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(ext_modules=cythonize(["Cython1.pyx",
+                            "Cython2.pyx",
+                            "Cython3.pyx",
+                            "Cython4.pyx",
+                            "Cython3_Test.pyx",
+                            "Cython4_Test.pyx"
+                            ]))
